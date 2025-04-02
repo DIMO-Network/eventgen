a536b7c929e0bb9baae500591cc60f0093a34b35
Dylan Moreland <dylan@dimo.zone>
2025-04-02 16:07:09 -0400

    New options
    * Output file, or stdout
    * Package name
    * Template file, or default struct and id set

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
+	"flag"
 	"fmt"
 	"go/format"
 	"io"
@@ -21,21 +23,35 @@ type Config struct {
 	Events []string `yaml:"events"`
 }
 
+//go:embed default.tmpl
+var defaultTemplate string
+
 func main() {
-	if len(os.Args) != 3 {
-		log.Fatal("Must supply a configuration file and a template.")
+	if len(os.Args) < 2 {
+		log.Fatal("Must supply an event configuration file.")
 	}
 
 	configPath := os.Args[1]
-	templatePath := os.Args[2]
+
+	fs := flag.NewFlagSet("", flag.ExitOnError)
+
+	tmplPath := fs.String("t", "", "Path to a template file. If not specified then a default struct template will be used.")
+	pkg := fs.String("p", "main", "Package name for the generated code. Defaults to main.")
+	out := fs.String("o", "", "Output path. If empty then printed to stdout.")
+
+	err := fs.Parse(os.Args[2:])
+	if err != nil {
+		// Shouldn't get here?
+		log.Fatalf("Failed to parse flags: %s", err)
+	}
 
 	f, err := os.Open(configPath)
 	if err != nil {
 		log.Fatalf("Error opening config file: %v.", err)
 	}
-	defer f.Close()
 
 	b, err := io.ReadAll(f)
+	f.Close()
 	if err != nil {
 		log.Fatalf("Error reading config file contents: %v.", err)
 	}
@@ -52,14 +68,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error opening ABI file: %v.", err)
 	}
-	defer f.Close()
 
 	ab, err := abi.JSON(f)
+	f.Close()
 	if err != nil {
 		log.Fatalf("Error parsing ABI file: %v.", err)
 	}
 
-	var events []TEvent
+	inp := TOverall{
+		Package: *pkg,
+	}
 
 	for _, e := range c.Events {
 		h := crypto.Keccak256Hash([]byte(e))
@@ -82,30 +100,52 @@ func main() {
 			})
 		}
 
-		events = append(events, TEvent{
+		inp.Events = append(inp.Events, TEvent{
 			Name:      ev.Name,
 			Arguments: args,
 			ID:        ev.ID,
 		})
 	}
 
-	t, err := template.ParseFiles(templatePath)
-	if err != nil {
-		log.Fatal(err)
+	tmpl := template.New("")
+	if *tmplPath == "" {
+		_, err := tmpl.Parse(defaultTemplate)
+		if err != nil {
+			log.Fatalf("Couldn't parse default template. This should never happen.")
+		}
+	} else {
+		_, err := tmpl.ParseFiles(*tmplPath)
+		if err != nil {
+			log.Fatalf("Couldn't parse template at %q.", *tmplPath)
+		}
 	}
 
 	var buf bytes.Buffer
-	err = t.Execute(&buf, events)
+	err = tmpl.Execute(&buf, inp)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	out, err := format.Source(buf.Bytes())
+	last, err := format.Source(buf.Bytes())
 	if err != nil {
 		log.Fatalf("Failed to format: %v", err)
 	}
 
-	fmt.Println(string(out))
+	var writ io.WriteCloser
+	if *out == "" {
+		writ = os.Stdout
+	} else {
+		writ, err = os.Create(*out)
+		if err != nil {
+			log.Fatalf("Failed to open output file %q: %s", *out, err)
+		}
+	}
+
+	_, err = writ.Write(last)
+	writ.Close()
+	if err != nil {
+		log.Fatalf("Failed to write output: %s", err)
+	}
 }
 
 // go-ethereum's abi package has a toGoType which is, unfortunately, private.
@@ -123,6 +163,11 @@ func solidityTypeToGo(s string) (string, error) {
 		return "[]byte", nil
 	}
 	return "", fmt.Errorf("we don't support Solidity type %s", s)
+}
+
+type TOverall struct {
+	Package string
+	Events  []TEvent
 }
 
 type TArgument struct {
